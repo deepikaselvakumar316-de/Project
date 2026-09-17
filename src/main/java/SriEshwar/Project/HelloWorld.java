7ed8fdd91d0a891616436dbf2d81fc8ef53e0854
deepikas2025aids <deepika.s2025aids@sece.ac.in>
2026-09-17 09:18:33 +0530

    Initial commit

---
+++ src/main/java/SriEshwar/Project/HelloWorld.java
@@ -0,0 +1,13 @@
+package SriEshwar.Project;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/")
+    public String getMethodName() {
+        return "Hello World";
+    }
+}
